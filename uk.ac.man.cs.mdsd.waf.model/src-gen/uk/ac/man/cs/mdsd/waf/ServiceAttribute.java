b1633b466ade5a062e7af48f51fb94b08ce3d59b
Andy Carpenter <Andy.Carpenter@manchester.ac.uk>
2015-09-03 14:24:34 +0100

    Remove legacy view concept

---
+++ uk.ac.man.cs.mdsd.waf.model/src-gen/uk/ac/man/cs/mdsd/waf/ServiceAttribute.java
@@ -0,0 +1,184 @@
+/**
+ */
+package uk.ac.man.cs.mdsd.waf;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Service Attribute</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * </p>
+ * <ul>
+ *   <li>{@link uk.ac.man.cs.mdsd.waf.ServiceAttribute#isCaseInsensitive <em>Case Insensitive</em>}</li>
+ *   <li>{@link uk.ac.man.cs.mdsd.waf.ServiceAttribute#isEncrypt <em>Encrypt</em>}</li>
+ *   <li>{@link uk.ac.man.cs.mdsd.waf.ServiceAttribute#getInterfaceType <em>Interface Type</em>}</li>
+ *   <li>{@link uk.ac.man.cs.mdsd.waf.ServiceAttribute#getPlaceholder <em>Placeholder</em>}</li>
+ *   <li>{@link uk.ac.man.cs.mdsd.waf.ServiceAttribute#getValidationPattern <em>Validation Pattern</em>}</li>
+ *   <li>{@link uk.ac.man.cs.mdsd.waf.ServiceAttribute#getDateFormat <em>Date Format</em>}</li>
+ * </ul>
+ *
+ * @see uk.ac.man.cs.mdsd.waf.WafPackage#getServiceAttribute()
+ * @model
+ * @generated
+ */
+public interface ServiceAttribute extends ServiceFeature, IncludedAttribute, UnitTitle {
+	/**
+	 * Returns the value of the '<em><b>Case Insensitive</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Case Insensitive</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Case Insensitive</em>' attribute.
+	 * @see #setCaseInsensitive(boolean)
+	 * @see uk.ac.man.cs.mdsd.waf.WafPackage#getServiceAttribute_CaseInsensitive()
+	 * @model unique="false"
+	 * @generated
+	 */
+	boolean isCaseInsensitive();
+
+	/**
+	 * Sets the value of the '{@link uk.ac.man.cs.mdsd.waf.ServiceAttribute#isCaseInsensitive <em>Case Insensitive</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Case Insensitive</em>' attribute.
+	 * @see #isCaseInsensitive()
+	 * @generated
+	 */
+	void setCaseInsensitive(boolean value);
+
+	/**
+	 * Returns the value of the '<em><b>Encrypt</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Encrypt</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Encrypt</em>' attribute.
+	 * @see #setEncrypt(boolean)
+	 * @see uk.ac.man.cs.mdsd.waf.WafPackage#getServiceAttribute_Encrypt()
+	 * @model unique="false"
+	 * @generated
+	 */
+	boolean isEncrypt();
+
+	/**
+	 * Sets the value of the '{@link uk.ac.man.cs.mdsd.waf.ServiceAttribute#isEncrypt <em>Encrypt</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Encrypt</em>' attribute.
+	 * @see #isEncrypt()
+	 * @generated
+	 */
+	void setEncrypt(boolean value);
+
+	/**
+	 * Returns the value of the '<em><b>Interface Type</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Interface Type</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Interface Type</em>' attribute.
+	 * @see #setInterfaceType(String)
+	 * @see uk.ac.man.cs.mdsd.waf.WafPackage#getServiceAttribute_InterfaceType()
+	 * @model unique="false"
+	 * @generated
+	 */
+	String getInterfaceType();
+
+	/**
+	 * Sets the value of the '{@link uk.ac.man.cs.mdsd.waf.ServiceAttribute#getInterfaceType <em>Interface Type</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Interface Type</em>' attribute.
+	 * @see #getInterfaceType()
+	 * @generated
+	 */
+	void setInterfaceType(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Placeholder</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Placeholder</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Placeholder</em>' attribute.
+	 * @see #setPlaceholder(String)
+	 * @see uk.ac.man.cs.mdsd.waf.WafPackage#getServiceAttribute_Placeholder()
+	 * @model unique="false"
+	 * @generated
+	 */
+	String getPlaceholder();
+
+	/**
+	 * Sets the value of the '{@link uk.ac.man.cs.mdsd.waf.ServiceAttribute#getPlaceholder <em>Placeholder</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Placeholder</em>' attribute.
+	 * @see #getPlaceholder()
+	 * @generated
+	 */
+	void setPlaceholder(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Validation Pattern</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Validation Pattern</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Validation Pattern</em>' attribute.
+	 * @see #setValidationPattern(String)
+	 * @see uk.ac.man.cs.mdsd.waf.WafPackage#getServiceAttribute_ValidationPattern()
+	 * @model unique="false"
+	 * @generated
+	 */
+	String getValidationPattern();
+
+	/**
+	 * Sets the value of the '{@link uk.ac.man.cs.mdsd.waf.ServiceAttribute#getValidationPattern <em>Validation Pattern</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Validation Pattern</em>' attribute.
+	 * @see #getValidationPattern()
+	 * @generated
+	 */
+	void setValidationPattern(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Date Format</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Date Format</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Date Format</em>' attribute.
+	 * @see #setDateFormat(String)
+	 * @see uk.ac.man.cs.mdsd.waf.WafPackage#getServiceAttribute_DateFormat()
+	 * @model unique="false"
+	 * @generated
+	 */
+	String getDateFormat();
+
+	/**
+	 * Sets the value of the '{@link uk.ac.man.cs.mdsd.waf.ServiceAttribute#getDateFormat <em>Date Format</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Date Format</em>' attribute.
+	 * @see #getDateFormat()
+	 * @generated
+	 */
+	void setDateFormat(String value);
+
+} // ServiceAttribute

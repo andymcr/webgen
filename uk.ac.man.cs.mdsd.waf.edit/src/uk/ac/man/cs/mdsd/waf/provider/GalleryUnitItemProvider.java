09b32fe0d1489d5a12546dee3f19250c0688a25f
andymcr <Andy Carpenter>
2017-01-12 11:29:13 +0000

    Make image index unit a type of image unit

--- uk.ac.man.cs.mdsd.waf.edit/src/uk/ac/man/cs/mdsd/waf/provider/GalleryUnitItemProvider.java
+++ uk.ac.man.cs.mdsd.waf.edit/src/uk/ac/man/cs/mdsd/waf/provider/GalleryUnitItemProvider.java
@@ -8,13 +8,9 @@ import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
-
-import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
-
-import org.eclipse.emf.edit.provider.ViewerNotification;
 import uk.ac.man.cs.mdsd.waf.GalleryUnit;
-import uk.ac.man.cs.mdsd.waf.WafFactory;
 import uk.ac.man.cs.mdsd.waf.WafPackage;
 
 /**
@@ -45,38 +41,31 @@ public class GalleryUnitItemProvider extends ImageUnitItemProvider {
 		if (itemPropertyDescriptors == null) {
 			super.getPropertyDescriptors(object);
 
+			addFullSizeFilterPropertyDescriptor(object);
 		}
 		return itemPropertyDescriptors;
 	}
 
 	/**
-	 * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
-	 * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
-	 * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
+	 * This adds a property descriptor for the Full Size Filter feature.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
-	@Override
-	public Collection<? extends EStructuralFeature> getChildrenFeatures(Object object) {
-		if (childrenFeatures == null) {
-			super.getChildrenFeatures(object);
-			childrenFeatures.add(WafPackage.Literals.INLINE_ACTION_CONTAINER__ACTIONS);
-		}
-		return childrenFeatures;
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	protected EStructuralFeature getChildFeature(Object object, Object child) {
-		// Check the type of the specified child object and return the proper feature to use for
-		// adding (see {@link AddCommand}) it as a child.
-
-		return super.getChildFeature(object, child);
+	protected void addFullSizeFilterPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_GalleryUnit_fullSizeFilter_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_GalleryUnit_fullSizeFilter_feature", "_UI_GalleryUnit_type"),
+				 WafPackage.Literals.GALLERY_UNIT__FULL_SIZE_FILTER,
+				 true,
+				 false,
+				 true,
+				 null,
+				 null,
+				 null));
 	}
 
 	/**
@@ -115,12 +104,6 @@ public class GalleryUnitItemProvider extends ImageUnitItemProvider {
 	@Override
 	public void notifyChanged(Notification notification) {
 		updateChildren(notification);
-
-		switch (notification.getFeatureID(GalleryUnit.class)) {
-			case WafPackage.GALLERY_UNIT__ACTIONS:
-				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
-				return;
-		}
 		super.notifyChanged(notification);
 	}
 
@@ -134,21 +117,6 @@ public class GalleryUnitItemProvider extends ImageUnitItemProvider {
 	@Override
 	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
 		super.collectNewChildDescriptors(newChildDescriptors, object);
-
-		newChildDescriptors.add
-			(createChildParameter
-				(WafPackage.Literals.INLINE_ACTION_CONTAINER__ACTIONS,
-				 WafFactory.eINSTANCE.createSelectAction()));
-
-		newChildDescriptors.add
-			(createChildParameter
-				(WafPackage.Literals.INLINE_ACTION_CONTAINER__ACTIONS,
-				 WafFactory.eINSTANCE.createDeleteAction()));
-
-		newChildDescriptors.add
-			(createChildParameter
-				(WafPackage.Literals.INLINE_ACTION_CONTAINER__ACTIONS,
-				 WafFactory.eINSTANCE.createFeatureSupportAction()));
 	}
 
 	/**

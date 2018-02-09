a067ab0caa12b5dd2eae542a7af2646df6f35fc1
andymcr <Andy Carpenter>
2018-02-09 23:42:11 +0000

    Allow suffixes on expressions

--- uk.ac.man.cs.mdsd.service.model/src-gen/uk/ac/man/cs/mdsd/service/impl/ParameterReferenceImpl.java
+++ uk.ac.man.cs.mdsd.service.model/src-gen/uk/ac/man/cs/mdsd/service/impl/ParameterReferenceImpl.java
@@ -2,13 +2,19 @@
  */
 package uk.ac.man.cs.mdsd.service.impl;
 
+import java.util.Collection;
+
 import org.eclipse.emf.common.notify.Notification;
+
+import org.eclipse.emf.common.util.EList;
 
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.InternalEObject;
 
 import org.eclipse.emf.ecore.impl.ENotificationImpl;
 import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+
+import org.eclipse.emf.ecore.util.EDataTypeEList;
 
 import uk.ac.man.cs.mdsd.service.FormalParameter;
 import uk.ac.man.cs.mdsd.service.ParameterReference;
@@ -22,12 +28,23 @@ import uk.ac.man.cs.mdsd.service.ServicePackage;
  * The following features are implemented:
  * </p>
  * <ul>
+ *   <li>{@link uk.ac.man.cs.mdsd.service.impl.ParameterReferenceImpl#getSuffixes <em>Suffixes</em>}</li>
  *   <li>{@link uk.ac.man.cs.mdsd.service.impl.ParameterReferenceImpl#getParameter <em>Parameter</em>}</li>
  * </ul>
  *
  * @generated
  */
 public class ParameterReferenceImpl extends MinimalEObjectImpl.Container implements ParameterReference {
+	/**
+	 * The cached value of the '{@link #getSuffixes() <em>Suffixes</em>}' attribute list.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getSuffixes()
+	 * @generated
+	 * @ordered
+	 */
+	protected EList<String> suffixes;
+
 	/**
 	 * The cached value of the '{@link #getParameter() <em>Parameter</em>}' reference.
 	 * <!-- begin-user-doc -->
@@ -55,6 +72,18 @@ public class ParameterReferenceImpl extends MinimalEObjectImpl.Container impleme
 	@Override
 	protected EClass eStaticClass() {
 		return ServicePackage.Literals.PARAMETER_REFERENCE;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EList<String> getSuffixes() {
+		if (suffixes == null) {
+			suffixes = new EDataTypeEList<String>(String.class, this, ServicePackage.PARAMETER_REFERENCE__SUFFIXES);
+		}
+		return suffixes;
 	}
 
 	/**
@@ -103,6 +132,8 @@ public class ParameterReferenceImpl extends MinimalEObjectImpl.Container impleme
 	@Override
 	public Object eGet(int featureID, boolean resolve, boolean coreType) {
 		switch (featureID) {
+			case ServicePackage.PARAMETER_REFERENCE__SUFFIXES:
+				return getSuffixes();
 			case ServicePackage.PARAMETER_REFERENCE__PARAMETER:
 				if (resolve) return getParameter();
 				return basicGetParameter();
@@ -115,9 +146,14 @@ public class ParameterReferenceImpl extends MinimalEObjectImpl.Container impleme
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public void eSet(int featureID, Object newValue) {
 		switch (featureID) {
+			case ServicePackage.PARAMETER_REFERENCE__SUFFIXES:
+				getSuffixes().clear();
+				getSuffixes().addAll((Collection<? extends String>)newValue);
+				return;
 			case ServicePackage.PARAMETER_REFERENCE__PARAMETER:
 				setParameter((FormalParameter)newValue);
 				return;
@@ -133,6 +169,9 @@ public class ParameterReferenceImpl extends MinimalEObjectImpl.Container impleme
 	@Override
 	public void eUnset(int featureID) {
 		switch (featureID) {
+			case ServicePackage.PARAMETER_REFERENCE__SUFFIXES:
+				getSuffixes().clear();
+				return;
 			case ServicePackage.PARAMETER_REFERENCE__PARAMETER:
 				setParameter((FormalParameter)null);
 				return;
@@ -148,10 +187,28 @@ public class ParameterReferenceImpl extends MinimalEObjectImpl.Container impleme
 	@Override
 	public boolean eIsSet(int featureID) {
 		switch (featureID) {
+			case ServicePackage.PARAMETER_REFERENCE__SUFFIXES:
+				return suffixes != null && !suffixes.isEmpty();
 			case ServicePackage.PARAMETER_REFERENCE__PARAMETER:
 				return parameter != null;
 		}
 		return super.eIsSet(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String toString() {
+		if (eIsProxy()) return super.toString();
+
+		StringBuffer result = new StringBuffer(super.toString());
+		result.append(" (suffixes: ");
+		result.append(suffixes);
+		result.append(')');
+		return result.toString();
 	}
 
 } //ParameterReferenceImpl
